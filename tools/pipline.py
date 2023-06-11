69d0196a8c4f3da72da1a7bf2d443a8832eb17f3
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-11 18:55:41 +0300

    feat: added SetupPipline in pipline.py

--- tools/pipline.py
+++ tools/pipline.py
@@ -1,6 +1,29 @@
-def main():
-    pass
+import os
+import torch
+import random
 
 
-if __name__ == "__main__":
-    main()
+class SetupPipline:
+    @staticmethod
+    def pycocotools_setup() -> None:
+        if not os.path.exists("/kaggle/working/packages"):
+            os.mkdir("/kaggle/working/packages")
+            os.system("cp -r /kaggle/input/pycocotools/* /kaggle/working/packages")
+            os.chdir("/kaggle/working/packages/pycocotools-2.0.6/")
+            os.system("python setup.py install")
+            os.system("pip install . --no-index --find-links /kaggle/working/packages/")
+            os.chdir("/kaggle/working")
+
+    @staticmethod
+    def seed_everything(seed: int) -> None:
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+
+    def __call__(self, seed: int = 42, pycoco: bool = True) -> None:
+        if pycoco:
+            self.pycocotools_setup()
+        if seed:
+            self.seed_everything(seed)
