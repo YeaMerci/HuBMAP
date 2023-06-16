e53d639cd04b25427d034a9866318b62d52fbcde
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-16 22:00:44 +0300

    feat: added argparse as CLI in dsimagegen.py

--- dsimagegen.py
+++ dsimagegen.py
@@ -1,10 +1,28 @@
 from dsimage import ImageWriter
+from argparse import ArgumentParser
 
 
-def main():
+def main(dirpath: str, filename: str):
     writer = ImageWriter()
-    writer.generate_sample()
+    writer.generate_sample(dirpath, filename)
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser(
+        description="Specify two arguments: "
+                    "1) dirpath - path to the directory in which the image file will be saved "
+                    "2) filename - image file name. Default value is sample_image.yaml"
+    )
+
+    parser.add_argument(
+        "dirpath", type=str,
+        help="dirpath - path to the directory in which the image file will be saved"
+    )
+
+    parser.add_argument(
+        "filename", type=str,
+        help="filename - image file name. Default value is sample_image.yaml"
+    )
+
+    args = parser.parse_args()
+    main(args.dirpath, args.filename)
