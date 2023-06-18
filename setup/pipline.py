c58c8e4eeb0e97af869037f115c0a62ff7a728ca
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-18 03:11:51 +0300

    feat: added methods __init__() & set_variable()

--- setup/pipline.py
+++ setup/pipline.py
@@ -1,13 +1,27 @@
 import os
 import torch
+import numpy as np
 import random
 
 
 class SetupPipline:
+    def __init__(self):
+        self.__root_dirpath = "/home/merci/PycharmProjects/competitions/HuBMAP/"
+
+        self.__annot_path = os.path.join(
+            self.__root_dirpath,
+            "data/hubmap-hacking-the-human-vasculature/polygons.jsonl"
+        )
+
+        self.__image_path = os.path.join(
+            self.__root_dirpath,
+            "data/hubmap-hacking-the-human-vasculature/train"
+        )
+
     @staticmethod
     def pycocotools_setup() -> None:
         if not os.path.exists("/kaggle/working/packages"):
-            os.mkdir("/kaggle/working/packages")
+            os.mkdirs("/kaggle/working/packages")
             os.system("cp -r /kaggle/input/pycocotools/* /kaggle/working/packages")
             os.chdir("/kaggle/working/packages/pycocotools-2.0.6/")
             os.system("python setup.py install")
@@ -22,8 +36,7 @@ class SetupPipline:
         torch.cuda.manual_seed(seed)
         torch.backends.cudnn.deterministic = True
 
-    def __call__(self, seed: int = 42, pycoco: bool = True) -> None:
-        if pycoco:
-            self.pycocotools_setup()
-        if seed:
-            self.seed_everything(seed)
+    def set_variable(self):
+        os.environ["ANNOT_PATH"] = self.__annot_path
+        os.environ["IMAGE_PATH"] = self.__image_path
+        os.environ["ROOT_DIRPATH"] = self.__root_dirpath
