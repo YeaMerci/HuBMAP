13f93fd1eb7a1b0b8bdab4f9906002f38e2cccbf
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-19 14:41:17 +0300

    feat: updated DataModuleImage class

--- imagen/images/datamodule_image.py
+++ imagen/images/datamodule_image.py
@@ -7,26 +7,15 @@ class DataModuleImage(ImageWriter):
     def __init__(self):
         self._filename = "sample_datamodule_image.yaml"
 
-        self.main_struct: dict = {
-            "head": {},
-            "body": {}
-        }
-
-        self.body = {
+        self.main_struct = {
             "train_size": 0.85,
             "batch_size": 4,
             "num_workers": 4,
-            "random_state": None
-        }
-
-        self.head = {
+            "random_state": None,
             "target_path": "some/path/to/annotations",
             "data_path": "some/path/to/images",
-            "config_path": "some/path/to/config.yaml",
+            "config_path": "some/path/to/config.yaml"
         }
 
     def generate_pattern(self) -> dict:
-        stem = self.main_struct.copy()
-        stem["head"] = self.head
-        stem["body"] = self.body
-        return stem
+        return self.main_struct.copy()
