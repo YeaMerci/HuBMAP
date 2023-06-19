ae53d5767ba6531938d566771335ca27779c18e9
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-19 14:44:21 +0300

    feat: added LightBuilder class

--- lightning/lightbuilder/lightbuilder.py
+++ lightning/lightbuilder/lightbuilder.py
@@ -1,6 +1,29 @@
-def main():
-    pass
+import os
+import yaml
 
 
-if __name__ == "__main__":
-    main()
+class LightBuilder:
+    def __init__(self, template_path: str):
+        self.__template_path = template_path
+        self.__check_path()
+        self._config = self.load_config()
+
+    def load_config(self) -> dict:
+        with open(self.__template_path, mode="r") as f:
+            data = yaml.load(stream=f, Loader=yaml.SafeLoader)
+        return data
+
+    def __check_path(self) -> None:
+        if not os.path.isfile(self.__template_path):
+            raise ValueError(
+                f"Filename was expected to be a path,"
+                f" but obtained: {self.__template_path}"
+            )
+
+        if "config" in self.__template_path.split("/"):
+            return None
+
+        elif "config" not in self.__template_path.split("/"):
+            print("Warning! The configuration file "
+                  "is not located in the root directory "
+                  "and may not be secure.")
