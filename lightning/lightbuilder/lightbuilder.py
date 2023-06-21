7d5a7dba383273bfcddf2187a2f68b095fc05e3b
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-06-21 05:34:06 +0300

    feat: added method __call__() for building project modules without initializing LightBuilder class

--- lightning/lightbuilder/lightbuilder.py
+++ lightning/lightbuilder/lightbuilder.py
@@ -3,27 +3,32 @@ import yaml
 
 
 class LightBuilder:
-    def __init__(self, template_path: str):
-        self.__template_path = template_path
-        self.__check_path()
-        self._config = self.load_config()
-
-    def load_config(self) -> dict:
-        with open(self.__template_path, mode="r") as f:
+    @staticmethod
+    def load_config(template_path) -> dict:
+        with open(template_path, mode="r") as f:
             data = yaml.load(stream=f, Loader=yaml.SafeLoader)
         return data
 
-    def __check_path(self) -> None:
-        if not os.path.isfile(self.__template_path):
+    @staticmethod
+    def __check_path(template_path: str) -> None:
+        if not os.path.isfile(template_path):
             raise ValueError(
                 f"Filename was expected to be a path,"
-                f" but obtained: {self.__template_path}"
+                f" but obtained: {template_path}"
             )
 
-        if "config" in self.__template_path.split("/"):
+        if "config" in template_path.split("/"):
             return None
 
-        elif "config" not in self.__template_path.split("/"):
+        elif "config" not in template_path.split("/"):
             print("Warning! The configuration file "
                   "is not located in the root directory "
                   "and may not be secure.")
+
+    def __call__(self,
+                 template_path: str,
+                 valid_path: bool = True
+                 ) -> dict:
+        if valid_path:
+            self.__check_path(template_path)
+        return self.load_config(template_path)
